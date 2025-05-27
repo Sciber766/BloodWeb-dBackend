94e321f28019a705f0a1a5ceefdfaf3dbf8dd53b
Naman Tiwari <www.skynetc52@gmail.com>
2025-05-27 17:31:08 +0530

    placement error 4

--- index.js
+++ index.js
@@ -31,23 +31,6 @@ app.get('/', (req, res) => {
   res.send('BloodWeb Backend Running');
 });
 
-
-// Setup Socket.IO connection handler
-io.on('connection', (socket) => {
-  console.log('User connected:', socket.id);
-  
-  socket.on('join', (userId) => {
-    socket.join(userId);
-    console.log(`User ${userId} joined room`);
-  });
-  
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
-  });
-});
-
-module.exports = { io };
-
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
